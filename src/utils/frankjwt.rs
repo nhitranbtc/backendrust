b2fa6e4546b9d674a79604b99fd488692fb17b5c
Nhi Tran <nhi.tran@hawking.network>
2020-04-26 10:19:11 +0700

    encode, decode, validate signature with type rs256

--- src/utils/frankjwt.rs
+++ src/utils/frankjwt.rs
@@ -1,8 +1,10 @@
 use crate::errors::ServiceError;
 use actix_web::HttpResponse;
 use chrono::{Duration, Local};
-use frank_jwt::{decode, encode, Algorithm, ValidationOptions};
+use frank_jwt::{decode, encode, validate_signature, Algorithm, ValidationOptions};
 use serde::{Deserialize, Serialize};
+use std::env;
+use std::path::PathBuf;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
@@ -35,16 +37,43 @@ impl Claims {
             company: company.into(),
             exp: (Local::now() + Duration::hours(24)).timestamp() as usize,
         }
-	}
-
+    }
 }
 
+#[allow(dead_code)]
 pub fn create_token(email: &str, company: &str) -> Result<String, HttpResponse> {
     let claims = json!(Claims::with_email(email, company));
     let secret = dotenv!("JWT_SECRET").to_string();
     let header = json!({});
     encode(header, &secret, &claims, Algorithm::HS256)
         .map_err(|e| HttpResponse::InternalServerError().json(e.to_string()))
+}
+
+pub fn create_token_rs256(email: &str, company: &str) -> Result<String, HttpResponse> {
+    let claims = json!(Claims::with_email(email, company));
+    let header = json!({});
+    let mut path = env::current_dir().unwrap();
+    path.push("test");
+    path.push("my_rsa_2048_key.pem");
+    path.to_str().unwrap().to_string();
+    encode(
+        header,
+        &get_rsa_256_private_key_full_path(),
+        &claims,
+        Algorithm::RS256,
+    )
+    .map_err(|e| HttpResponse::InternalServerError().json(e.to_string()))
+}
+
+pub fn validate_signature_jwt_rs256(jwt1: &str) {
+    let mut path = env::current_dir().unwrap();
+    path.push("test");
+    path.push("my_rsa_2048_key.pem");
+    path.to_str().unwrap().to_string();
+    let maybe_res =
+        validate_signature(&jwt1, &get_rsa_256_public_key_full_path(), Algorithm::RS256);
+    println!("maybe_res {:?}", maybe_res);
+    //assert!(maybe_res.unwrap());
 }
 
 pub fn decode_token(token: &str) -> Result<SlimUser, ServiceError> {
@@ -57,13 +86,13 @@ pub fn decode_token(token: &str) -> Result<SlimUser, ServiceError> {
     );
     match decoded {
         Ok(v) => {
-			let (_header, payload) = v;
-			//println!("header {}", header);
-			//println!("payload {}", payload);
+            let (_header, payload) = v;
+            //println!("header {}", header);
+            //println!("payload {}", payload);
             let email = payload["sub"].as_str().unwrap();
             let company = payload["company"].as_str().unwrap();
             let claims = Claims::with_email(email, company);
-			//println!("claims {:?}", claims);
+            //println!("claims {:?}", claims);
             let new_user = SlimUser::from(claims);
             Ok(new_user)
         }
@@ -71,11 +100,70 @@ pub fn decode_token(token: &str) -> Result<SlimUser, ServiceError> {
     }
 }
 
+pub fn decode_jwt_rs256(token: &str) -> Result<SlimUser, ServiceError> {
+    let decoded = decode(
+        &token,
+        &get_rsa_256_public_key_full_path(),
+        Algorithm::RS256,
+        &ValidationOptions::dangerous(),
+    );
+    match decoded {
+        Ok(v) => {
+            let (header, payload) = v;
+            println!("header \n{}", header);
+            //println!("payload {}", payload);
+            let email = payload["sub"].as_str().unwrap();
+            let company = payload["company"].as_str().unwrap();
+            let claims = Claims::with_email(email, company);
+            //println!("claims {:?}", claims);
+            let new_user = SlimUser::from(claims);
+            Ok(new_user)
+        }
+        Err(_e) => Err(ServiceError::Unauthorized),
+    }
+}
+
+fn get_rsa_256_public_key_full_path() -> PathBuf {
+    let mut path = env::current_dir().unwrap();
+    path.push("test");
+    path.push("my_rsa_public_2048_key.pem");
+    path.to_path_buf()
+}
+
+fn get_rsa_256_private_key_full_path() -> PathBuf {
+    let mut path = env::current_dir().unwrap();
+    path.push("test");
+    path.push("my_rsa_2048_key.pem");
+    path.to_path_buf()
+}
 
 #[allow(dead_code)]
 pub fn test_decode(token: &str) {
     //println!("token {:?}", token);
-	let test = decode_token(token);
-	println!("decode {:?}", test);
+    let test = decode_token(token);
+    println!("decode {:?}", test);
 }
 
+#[allow(dead_code)]
+pub fn test_encoded_rs256() {
+    let p1 = json!({
+        "key1" : "val1",
+        "key2" : "val2",
+        "key3" : "val3"
+    });
+
+    let header = json!({});
+    let mut path = env::current_dir().unwrap();
+    path.push("test");
+    path.push("my_rsa_2048_key.pem");
+    path.to_str().unwrap().to_string();
+
+    let jwt1 = encode(
+        header,
+        &get_rsa_256_private_key_full_path(),
+        &p1,
+        Algorithm::RS256,
+    )
+    .unwrap();
+    println!("jwt1 {}", jwt1);
+}
