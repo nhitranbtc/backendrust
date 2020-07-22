2f173cb2fd4d8cf6cc47b77bece255a65dc7d230
Nhi Tran <nhi.tran@hawking.network>
2020-07-22 08:53:24 +0700

    override default mapping properties

--- src/elastic_actor/model/mytype.rs
+++ src/elastic_actor/model/mytype.rs
@@ -10,14 +10,39 @@ pub struct MyType {
     pub id: String,
     pub title: String,
     pub generation: i64,
+    pub nested: MyNested,
+    pub object: MyObject,
     pub timestamp: Date<DefaultDateMapping>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct NewMyType {
     pub title: String,
-    pub generation: i64
+    pub generation: i64,
+    pub nested: MyNested,
+    pub object: MyObject
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize, ElasticType)]
+pub struct MyNested {
+    //pub my_date: Date<DefaultDateMapping>,
+    //pub my_string: String,
+    pub my_num: i32
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, ElasticType)]
+#[elastic(mapping="MyObject")]
+pub struct MyObject {
+    //pub my_date: Date<DefaultDateMapping>,
+    //pub my_string: String,
+    pub my_num: i32
+}
+
+impl ObjectMapping for MyObject {
+    type Properties = MyObject;
+    fn data_type() -> &'static str { OBJECT_DATATYPE }
+}
+
 
 impl MyType {
     #[allow(dead_code)]
@@ -32,12 +57,25 @@ impl MyType {
         mytype
     }
 
+    pub fn new() -> Self {
+        let data = json!({
+            "title":"Title ",
+            "generation": 0,
+            "nested": json!({"my_num": 1}),
+            "object": json!({"my_num": 1})
+        });
+        let mytype = Self::index_res(data);
+        mytype
+    }
+
     pub fn index_res(data: Value) -> Self {
         let mytype: NewMyType = serde_json::from_value(data).unwrap();
         Self {
             id: Uuid::new_v4().to_string(),
             title: mytype.title,
             generation: mytype.generation,
+            nested: mytype.nested,
+            object: mytype.object,
             timestamp: Date::now(),
         }
     }
